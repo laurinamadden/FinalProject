ff5e6273419285cbb8daec393103bf5fe8be273a
Laurina Madden <laurinamadden@gmail.com>
2025-11-02 16:24:38 +0000

    02.11.2025 - Push of last nights work

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -18,7 +18,9 @@
     //document.getElementById('myform').onsubmit = function(event){
     // apparently addEventListener is more modern and preferred and "best practice" as it lets me add multiple listeners to the same event, and is more flexible. according to Codecademy AI.
     // .addEventListener is a flexible approach and allows you to add multiple event listeners to the same element and does not overwrite any existing handlers.
-    document.getElementById('myform').addEventListener("submit"), function(event){
+    // 01.11.2025 - I had an extra bracket ("submit") - removed it today
+    
+    document.getElementById('myform').addEventListener("submit", function(event){
         event.preventDefault();
          
         // Test                                              
@@ -32,7 +34,20 @@
         const password = document.getElementById('password').value;
 */
         //01.11.2025
-        
+        const data = {
+            username: document.getElementById('username').value,
+            phone: document.getElementById('phone').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        };
+
+        // fetch() will send POST request with header Content-Type: application/json
+        fetch("/users",{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            // JSON.stringify() converts the data to JSON for server side
+            body: JSON.stringify(data)
+        });
 
         // Aprox 29.10.2025 - Validate data in client side 
 
@@ -45,21 +60,21 @@
         
 
 
-    };
+    });
 
 
-    function regFunction(){
+    //function regFunction(){
        
         // W3 Schools
         // const myObj = Object.fromEntries(fruits);
         
         // Test                                              
-        console.log("Inside the regFunction of JavaScript");
+       // console.log("Inside the regFunction of JavaScript");
 
 
 
 
 
-    }
+    //}
 
 
